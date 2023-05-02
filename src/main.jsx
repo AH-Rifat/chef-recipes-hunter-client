a46c50b0d3bb3e78fcef76a4709b11b7bad17838
ah-rifat <rifat266@gmail.com>
2023-05-02 20:58:59 +0600

    install toastify package  done

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,12 @@ import 'flowbite'
 import 'react-icons'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>,
 )
